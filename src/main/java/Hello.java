33fa81faef66f886a5c96a4021ca638a900184b1
xVladislavx <ledovskihvladislav@gmail.com>
2019-09-06 12:27:47 +0300

    Создать ветку (cherry) , в которой в классе HelloWorld будет существовать метод  выводящий текущую дату на экран, оправить коммит с веткой в удаленный репозиторий, в ветке мастера такого метода не должно быть.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,12 @@
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hi");
 
+    }
+    public static void Time(){
+        Date date = new Date();
+        System.out.println(date);
     }
 }
